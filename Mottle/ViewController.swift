3ca56aa1c46b59527fadb19f58f9613a0f1b5e6b
Hamza Bella <hbella@macd005-23.local>
2023-05-09 09:18:02 +0200

    Initial Commit

---
+++ Mottle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mottle
+//
+//  Created by Hamza Bella on 09/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
