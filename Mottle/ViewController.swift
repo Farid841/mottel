54dfb7402d0417e7e3eb2fc06628a77a9858c818
Farid Maman <fmaman@macd005-18.local>
2023-05-16 15:55:34 +0200

    Ajout de l'animation de chargement de m'application

--- Mottle/ViewController.swift
+++ Mottle/ViewController.swift
@@ -8,9 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageView: UIImageView = {
+        
+        let imageView = UIImageView(frame: CGRect(x:0, y:0, width:150, height: 150))
+        imageView.image = UIImage(named: "Gaming")
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(imageView)
+        DispatchQueue.main.asyncAfter(deadline: .now()+5) {
+            self.performSegue(withIdentifier: "HomeToGame", sender: self)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.center = view.center
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5){
+            self.animation()
+            
+        }
+    }
+    
+    func animation(){
+        UIView.animate(withDuration: 1){
+            let size = self.view.frame.size.width * 2
+            let xposition = size - self.view.frame.width - size
+            let yposition = self.view.frame.height - size
+            
+            self.imageView.frame = CGRect(x: -(xposition/2), y: yposition, width: size, height: size)
+            self.imageView.alpha = 0
+        }
     }
     
      override func prepare(for segue: UIStoryboardSegue,sender: Any?){
